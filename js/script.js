8be3a8d75bd97999aad5d1968cf723aca6e0c303
Gleb <vishnyagleb2@gmail.com>
2022-08-11 01:49:00 +0300

    functions add

--- js/script.js
+++ js/script.js
@@ -168,3 +168,129 @@ function getMathResult(base, iter) {
     }
 }
 console.log(getMathResult(10, 0)); */
+
+//калькулятор площади всех плоскостей и обьема куба
+
+/* function calculateVolumeAndArea(lengthRibCube) {
+    if (Number.isInteger(lengthRibCube) && lengthRibCube >= 0) {
+        let squere = Math.round(lengthRibCube) * Math.round(lengthRibCube) * 6,
+            volume = Math.pow(Math.round(lengthRibCube), 3);
+        console.log(`Объем куба: ${volume}, площадь всей поверхности: ${squere}`);
+    } else {
+        console.log('При вычислении произошла ошибка');
+    }
+}
+calculateVolumeAndArea(0); */
+
+//Найти купе по номеру места
+
+/* function getCoupeNumber(num) {
+    if (Number.isInteger(num) && num > 0 && num <= 36) {
+        let coupeNumber = Math.ceil(Math.round(num) / 4);
+        console.log(coupeNumber);
+    } else if(Number.isInteger(num) && (num === 0 || num > 36)){
+        console.log("Таких мест в вагоне не существует");
+    } else {
+        console.log("Ошибка. Проверьте правильность введенного номера места");
+    }
+}
+getCoupeNumber(6.35); */
+
+//Минуты в часы и минуты
+
+/* function getTimeFromMinutes(minutes) {
+    if (minutes < 0 || !Number.isInteger(minutes)) {
+        return "Ошибка, проверьте данные";
+    }
+    let hours = Math.floor(minutes / 60),
+        minut = minutes % 60,
+        hour,
+        minu = ` и ${minut} минут`;
+    if (hours === 0 || hours >= 5) {
+        hour = `Это ${hours} часов`;
+    } else if(hours === 1){
+        hour = `Это ${hours} час`;
+    } else {
+        hour = `Это ${hours} часа`;
+    }
+    return hour + minu;
+}
+getTimeFromMinutes(456); */
+
+//поиск большего числа
+
+/* function findMaxNumber(num1, num2, num3, num4) {
+    let maxNumbers = [num1, num2, num3, num4];
+    if (typeof (num1, num2, num3, num4) != 'number' || maxNumbers.length < 4) {
+        return 0;
+    }
+    return Math.max(num1, num2, num3, num4);
+}
+findMaxNumber(1, 2, 3,4); */
+
+// прикол с Фибоначчи
+
+/* function fib(num) {
+    if (!Number.isInteger(num) || num <= 0) {
+        return ``;
+    }
+    if (num => 3) {
+           let numbsFib = [0, 1];
+        for (let i = 2; i < num; i++){
+            numbsFib[i] = numbsFib[i - 2] + numbsFib[i - 1];
+        }
+        let result = '';
+        for (let i = 0; i < num-1; i++){
+            result += `${numbsFib[i]} `;
+        }
+        return result += `${numbsFib[num-1]}`;
+    }
+}
+fib(7); */
+/* const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+console.log(option.name);
+
+delete options.name;  */
+const personalPlanPeter = {
+    name: "Peter",
+    age: "30",
+    skills: {
+        languages: ['ru', 'ua'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs() {
+            console.log (`Мне ${personalPlanPeter.age} и я владею языками: ${(personalPlanPeter.skills.languages.join(" ")).toUpperCase()}`);
+    }
+};
+
+function showExperience(plan) {
+    return plan.skills.exp;
+}
+showExperience(personalPlanPeter);
+function showProgrammingLangs(plan) {
+    let langs = '';
+    if (plan.skills.programmingLangs.length !== 0) {
+        for (let key in plan.skills.programmingLangs) {
+            langs += `Язык ${key} изучен на ${plan.skills.programmingLangs[key]}\n`;
+        }
+    }
+    return langs;
+}
+showProgrammingLangs(personalPlanPeter);
+personalPlanPeter.showAgeAndLangs();
+function sayHI() {
+  console.log("Hello, this is my first function!");
+}
+sayHi();
